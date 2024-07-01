7bf3deac5588be8ef52685acb7e9701510710fd3
Reful4ent <dima2323fr3@gmail.com>
2024-07-02 00:59:01 +0700

    Redacted GameSettings

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,36 @@ import {useState} from 'react'
 import CanvasField from "./CanvasField.jsx";
 import GameButtons from "./GameButtons.jsx";
 import SocialLinks from "./SocialLinks.jsx";
+import GameSettings from "./GameSettings.jsx";
 export default function Game() {
-    const [sizeField, setSizeField] = useState(7);
+    const [sizeField, setSizeField] = useState(5);
     const [isPlaying, setIsPlaying] = useState(false);
     const [speedGame, setSpeedGame] = useState(10);
+    const [speedSliderGame, setSpeedSliderGame] = useState(10);
     const [isDraw,setIsDraw] = useState(true);
+    const [isClosed, setIsClosed] = useState(true);
+    const maxSpeed = 400;
+    const minSpeed = 10;
 
     function handleSizeChanged(e) {
         setSizeField(e.target.value);
+    }
+
+    function handleClosedClick() {
+        setIsClosed(!isClosed);
+    }
+
+    function handleSpeedChanged(e) {
+        if (e.target.value <= (maxSpeed - minSpeed) / 2){
+            setSpeedGame( maxSpeed - e.target.value + 10);
+            setSpeedSliderGame(e.target.value);
+            console.log(maxSpeed - e.target.value + 10);
+        }
+        else {
+            console.log(Math.abs(e.target.value - maxSpeed) + 10);
+            setSpeedGame(Math.abs(e.target.value - maxSpeed) + 10);
+            setSpeedSliderGame(e.target.value)
+        }
     }
 
 
@@ -40,10 +62,16 @@ export default function Game() {
 
     return (
         <>
+            <GameSettings speedGame={speedSliderGame}
+                          maxSpeed={maxSpeed}
+                          minSpeed={minSpeed}
+                          sizeField={sizeField}
+                          handleSizeChanged={handleSizeChanged}
+                          handleSpeedChanged={handleSpeedChanged} isClosed={isClosed} handleClosedClick={handleClosedClick}></GameSettings>
             <header className="header">
                 <ul className="header__list">
                     <li className="list__item">
-                        <button className="list__item options">
+                        <button className="list__item options" onClick={handleClosedClick}>
                             <img src="/public/Img/menu.svg" className="options__menu"/></button>
                     </li>
                     <li className="list__item header-text">Game Of Life</li>
@@ -59,7 +87,7 @@ export default function Game() {
                                          sizeField={sizeField}></CanvasField>
                         </div>
                     </div>
-                    <div className="game-settings__game-buttons">
+                    <div className="game__game-buttons">
                         <GameButtons isPlaying={isPlaying}
                                      isDraw={isDraw}
                                      handleSteplick={handleSteplick}
--- src/GameSettings.jsx
+++ src/GameSettings.jsx
@@ -1,16 +1,22 @@
-export default function GameSettings({sizeField,handleSizeChanged,speedGame,handleSpeedChanged}) {
+export default function GameSettings({sizeField,handleSizeChanged,maxSpeed, minSpeed, speedGame,handleSpeedChanged, isClosed, handleClosedClick}) {
+    let value = isClosed ? "game__game-settings" : "game__game-settings-hidden";
     return (
         <>
-            <div className="game__game-settings">
+            <div className={value}>
+                <div className="game-settings-close">
+                    <button className="close__button" onClick={handleClosedClick}>
+                        <img src="/public/Img/close.svg" className="close__icon"/>
+                    </button>
+                </div>
                 <div className="game-settings__set-field">
                     <div className="set-field__size">
-                        <text className="count-text">размер сетки</text>
-                        <input type="range" className="field_slider" min={5} max={150} value={sizeField}
+                        <text className="count-text">Размер сетки</text>
+                        <input type="range" className="field_slider" min={7} max={150} value={sizeField}
                                onChange={handleSizeChanged}/>
                     </div>
                     <div className="set-field__speed">
-                        <text className="count-text">скорость изменения:</text>
-                        <input type="range" className="field_slider" min={10} max={500} value={speedGame}
+                        <text className="count-text">Скорость изменения:</text>
+                        <input type="range" className="field_slider" min={minSpeed} max={maxSpeed} value={speedGame}
                                onChange={handleSpeedChanged}/>
                     </div>
                 </div>
