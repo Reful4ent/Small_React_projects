9eac1eff79675f59c79ad8f826bd8aa8ed66cddb
Reful4ent <dima2323fr3@gmail.com>
2024-07-04 23:54:53 +0700

    Started working with Weater API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-
+import {useEffect, useState} from "react";
+import fetchCurrentWeather from "./shared/api/methods.jsx";
 const App = () => {
+    const [cityParams, setCityParams] = useState({
+        name: null,
+        lat: 0,
+        lon: 0,
+    })
+
+
+    useEffect(() => {
+        const data = fetchCurrentWeather("Moscow","RU", "ru").then((response) => {
+            console.log(response);
+            setCityParams({
+                name: response[0].name,
+                lat: response[0].lat,
+                lon: response[0].lon
+            });
+        });
+    },[])
+
     return (
         <>
-            <p>Sdasdas</p>
+            <p>{cityParams.lon}</p>
+            <p>{cityParams.lat}</p>
         </>
     )
 }
